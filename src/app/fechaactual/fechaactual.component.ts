48d50adb519bd001a2ba9494292a2d8320cfb571
Marti <jvasquez@softlabperu.com>
2018-08-21 15:20:26 -0500

    Formato Pipe i18nSelect

--- src/app/fechaactual/fechaactual.component.ts
+++ src/app/fechaactual/fechaactual.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-fehcaactual',
-  template: `<p>La cotizacion actual del dolar es de {{dolareuro | currency: 'EUR': true}}</p>`,
+  template: `<p>{{sexo | i18nSelect: encabezamiento}} {{nombre}}</p>`,
+  // template: `<p>La cotizacion actual del dolar es de {{dolareuro | currency: 'EUR': true}}</p>`,
 // template: `<p>El resultado es {{resultado | number: '3.2-2'}}</p>`,
 // template: `<p> {{ciudad | lowercase}}, {{hoy | date:'d-M-y'}} a las {{hoy | date: 'H:m  Z'}} </p>
 // <app-copyright></app-copyright>`,
@@ -14,5 +15,8 @@ export class FechaactualComponent {
   // hoy = new Date();
   // ciudad: string = 'Lima';
   // resultado: number = 1.148;
-  dolareuro: number = 0.88;
+  // dolareuro: number = 0.88;
+  nombre: string = 'Carlos';
+  sexo: string = 'hombre';
+  encabezamiento: any = {'hombre': 'Estimado', 'mujer': 'Estimada'};
 }
