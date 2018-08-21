83d2e1524bed3167595a85a5d8c9d9fd73017965
Marti <jvasquez@softlabperu.com>
2018-08-21 10:29:01 -0500

    Se agrego los modulos nuevos en el app.module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {FechaactualComponent} from './fechaactual/fechaactual.component';
-import { CopyrightComponent } from './copyright/copyright.component';
-import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
-import { EjmetodoComponent } from './ejmetodo/ejmetodo.component';
-import { EjpropertybindingComponent } from './ejpropertybinding/ejpropertybinding.component';
+import {CopyrightComponent} from './copyright/copyright.component';
+import {ViewmodeloComponent} from './viewmodelo/viewmodelo.component';
+import {EjmetodoComponent} from './ejmetodo/ejmetodo.component';
+import {EjpropertybindingComponent} from './ejpropertybinding/ejpropertybinding.component';
+import {EjeventbindingComponent} from './ejeventbinding/ejeventbinding.component';
+import {Ej2waybindingComponent} from './ej2waybinding/ej2waybinding.component';
+import { EjdirectivangifComponent } from './ejdirectivangif/ejdirectivangif.component';
+import { EjdirectivangstyleComponent } from './ejdirectivangstyle/ejdirectivangstyle.component';
+import { EjdirectivangclassComponent } from './ejdirectivangclass/ejdirectivangclass.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +21,16 @@ import { EjpropertybindingComponent } from './ejpropertybinding/ejpropertybindin
     CopyrightComponent,
     ViewmodeloComponent,
     EjmetodoComponent,
-    EjpropertybindingComponent
+    EjpropertybindingComponent,
+    EjeventbindingComponent,
+    Ej2waybindingComponent,
+    EjdirectivangifComponent,
+    EjdirectivangstyleComponent,
+    EjdirectivangclassComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
