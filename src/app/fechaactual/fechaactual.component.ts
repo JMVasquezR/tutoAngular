4500341e62dc99ce9c1b5da59bd1410f1b77baf8
Marti <jvasquez@softlabperu.com>
2018-08-21 15:10:27 -0500

    Formato Pipe decimal

--- src/app/fechaactual/fechaactual.component.ts
+++ src/app/fechaactual/fechaactual.component.ts
@@ -2,13 +2,15 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-fehcaactual',
-  template: `<p> {{ciudad | lowercase}}, {{hoy | date:'d-M-y'}} a las {{hoy | date: 'H:m  Z'}} </p>
-  <app-copyright></app-copyright>`,
-  // templateUrl: './fechaactual.component.html',
+  template: `<p>El resultado es {{resultado | number: '3.2-2'}}</p>`,
+// template: `<p> {{ciudad | lowercase}}, {{hoy | date:'d-M-y'}} a las {{hoy | date: 'H:m  Z'}} </p>
+// <app-copyright></app-copyright>`,
+// templateUrl: './fechaactual.component.html',
   styleUrls: ['./fechaactual.component.css']
 })
 
 export class FechaactualComponent {
-  hoy = new Date();
-  ciudad: string = 'Lima';
+  // hoy = new Date();
+  // ciudad: string = 'Lima';
+  resultado: number = 1.148;
 }
