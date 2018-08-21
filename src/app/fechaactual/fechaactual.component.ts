c1fbfa45c4a3c188aec7d0637817add57eca2e09
Marti <jvasquez@softlabperu.com>
2018-08-21 15:02:35 -0500

    Formato Pipe Date

--- src/app/fechaactual/fechaactual.component.ts
+++ src/app/fechaactual/fechaactual.component.ts
@@ -2,10 +2,12 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-fehcaactual',
-  templateUrl: './fechaactual.component.html',
+  template: `<p> Lima, {{hoy | date:'d-M-y'}} a las {{hoy | date: 'H:m  Z'}} </p>
+  <app-copyright></app-copyright>`,
+  // templateUrl: './fechaactual.component.html',
   styleUrls: ['./fechaactual.component.css']
 })
 
-export  class FechaactualComponent {
+export class FechaactualComponent {
   hoy = new Date();
 }
